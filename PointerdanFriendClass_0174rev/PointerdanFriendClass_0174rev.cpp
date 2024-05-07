9c4797b47ef18261cd8bda66ef19a7363050ddac
thekill022 <gibfathoni@gmail.com>
2024-05-07 12:07:55 +0700

    deklarasi variabel nim

--- PointerdanFriendClass_0174rev/PointerdanFriendClass_0174rev.cpp
+++ PointerdanFriendClass_0174rev/PointerdanFriendClass_0174rev.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 class Mahasiswa { //membuat class mahasiswa
 public: //memberi akses modifier public pada class Mahasiswa
+	int nim; //deklarasi variabel nim
 };
 
 int main()
