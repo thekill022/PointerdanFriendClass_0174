04d6fa9c312c9b58749b97dab2903aaefcf68d05
thekill022 <gibfathoni@gmail.com>
2024-05-07 12:25:13 +0700

    deklarasi prosedur showNilaiPelajar()

--- friendclass3/friendclass3.cpp
+++ friendclass3/friendclass3.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+class pelajar;
+
+class manusia {
+public:
+    void showNilaiPelajar(pelajar& x); //deklarasi prosedur showNilaiPelajar()
+};
+
 int main()
 {
  
