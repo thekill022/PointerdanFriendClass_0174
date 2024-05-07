8b9af9946bcc59a5c556f7cacfe59d9374acaede
thekill022 <gibfathoni@gmail.com>
2024-05-07 12:08:42 +0700

    membuat prosedur showNim()

--- PointerdanFriendClass_0174rev/PointerdanFriendClass_0174rev.cpp
+++ PointerdanFriendClass_0174rev/PointerdanFriendClass_0174rev.cpp
@@ -4,6 +4,9 @@ using namespace std;
 class Mahasiswa { //membuat class mahasiswa
 public: //memberi akses modifier public pada class Mahasiswa
 	int nim; //deklarasi variabel nim
+	void showNim() { //membuat prosedur showNim()
+		cout << "No Induk = " << nim << endl;
+	}
 };
 
 int main()
