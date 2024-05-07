7f789132b3bfb41f7bc7a961dec67e4220fc7242
thekill022 <gibfathoni@gmail.com>
2024-05-07 12:25:35 +0700

    membuat  object pelajar didalam class pelajar

--- friendclass3/friendclass3.cpp
+++ friendclass3/friendclass3.cpp
@@ -8,6 +8,16 @@ public:
     void showNilaiPelajar(pelajar& x); //deklarasi prosedur showNilaiPelajar()
 };
 
+class pelajar {
+private:
+    int nilai;
+public:
+    pelajar() { //membuat  object pelajar didalam class pelajar
+        nilai = 100;
+    }
+    friend void manusia::showNilaiPelajar(pelajar& x);
+};
+
 int main()
 {
  
