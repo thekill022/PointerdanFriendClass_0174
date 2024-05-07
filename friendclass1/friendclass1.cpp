82d2f125b5efcce3295e9eba0a6851571dc43788
thekill022 <gibfathoni@gmail.com>
2024-05-07 12:17:43 +0700

    deklarasi prosedur setNama dengan paramater dari class orang

--- friendclass1/friendclass1.cpp
+++ friendclass1/friendclass1.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 class siswa; //deklarasi class siswa
+class orang {
+private:
+    string nama;
+
+public:
+    void setNama(string pNama); //deklarasi prosedur setNama dengan paramater dari class orang
+};
 
 int main()
 {
