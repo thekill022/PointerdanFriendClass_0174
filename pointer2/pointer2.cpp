1b154529b25c44a204a46c442b356b2885f92145
thekill022 <gibfathoni@gmail.com>
2024-05-07 12:15:27 +0700

    delete mhs untuk menghapus pointer setelah program berhenti dijalankan

--- pointer2/pointer2.cpp
+++ pointer2/pointer2.cpp
@@ -16,5 +16,7 @@ int main()
 	Mahasiswa* mhs = new Mahasiswa{ 1 }; //memberi nilai pertama menggunakan new    
 	mhs->nim = 2;  // memberi nilai pada variebale nim dari objek mhs
 	mhs->showNim(); //memanggil prosedur showNim dari variabel mhs yang merupakan pointer dereference
+	delete mhs; // delete mhs untuk menghapus pointer setelah program berhenti dijalankan
+	return 0;
 
 }
